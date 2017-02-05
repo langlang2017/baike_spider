26c04966c7d9926e1428eb86c1eeace3e8e7c797
idiotdj <idiotdj913@gmail.com>
2017-02-05 16:24:20 +0800

    Initial commit

---
+++ spider_main.py
@@ -0,0 +1,36 @@
+import html_downloader
+import html_outputer
+import html_parser
+import url_manager
+
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def crawl(self, root_url):
+        count = 1
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            try:
+                new_url = self.urls.get_new_url()
+                print('crawl %d : %s' % (count, new_url))
+                html_cont = self.downloader.download(new_url)
+                new_urls, new_data = self.parser.parse(new_url, html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+                if count == 100:
+                    break
+                count = count + 1
+            except:
+                print('crawl failed')
+        self.outputer.output_html()
+
+
+if __name__=="__main__":
+    root_url = "http://baike.baidu.com/item/Python"
+    obj_spider = SpiderMain()
+    obj_spider.crawl(root_url)
